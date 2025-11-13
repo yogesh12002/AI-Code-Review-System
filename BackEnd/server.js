db10f528da9807c8513595afebcbe3d318d2133a
Yogesh Gupta <yogeshgupta12002@gmail.com>
2025-11-13 23:35:18 +0530

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 // Import the Express app from the src/app.js file
 const app = require('./src/app');
 
+
 // Start the server and listen on port 3000
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
